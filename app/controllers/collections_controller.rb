74581d67eaac3c042927f217be28697bb2765d31
Luis Prill Sempere <luniprise@gmail.com>
2015-02-04 15:57:09 +0100

    added creation method + params permission + prepared user_id

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -8,4 +8,16 @@ class CollectionsController < ApplicationController
     @collection = Collection.new
   end
   
+  def create
+    @collection = Collection.new(colleciton_params)
+    @collection.save
+    redirect_to collections_path
+  end
+  
+  private
+  
+  def colleciton_params
+    params.require(:collection).permit(:name, :user_id)
+  end
+  
 end
