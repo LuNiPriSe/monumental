0858ffefa5a85aa9ab0725d90239341260f3963f
Luis Prill Sempere <luniprise@gmail.com>
2015-02-04 17:54:10 +0100

    pending specs

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Collection, type: :model do
   pending "check validation of user_id"
   pending "check validation of name"
+  pending "check dependent destroy of monuments"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   pending "check user registration"
   pending "check user login"
-  pending "check user logout" 
+  pending "check user logout"
+  pending "check dependent destroy of collections" 
 end
