1003ff62a0e1dc154129648489f768f7d2ed6a82
Luis Prill Sempere <luniprise@gmail.com>
2015-02-04 16:50:00 +0100

    generated monuments controller + copied collection methods (BROKEN)

---
+++ app/controllers/monuments_controller.rb
@@ -0,0 +1,39 @@
+class MonumentsController < ApplicationController
+  
+  def index
+    @collections = current_user.collections
+  end
+  
+  def new
+    @collection = Collection.new(user_id: current_user.id)   # seems safer to add here than in view
+  end
+  
+  def create
+    @collection = Collection.new(colleciton_params)
+    if @collection.save
+      redirect_to collections_path, notice: 'You created succesfully a collection.'
+    else
+      render action: 'new'
+    end
+  end
+  
+  def edit
+    @collection = Collection.find(params[:id])
+  end
+  
+  def update
+    @collection = Collection.find(params[:id])
+    if @collection.update(colleciton_params)
+      redirect_to collections_path, notice: 'You updated your collection successfully.'
+    else
+      render action: 'edit'
+    end
+  end
+  
+  private
+  
+  def colleciton_params
+    params.require(:collection).permit(:name, :user_id)
+  end
+  
+end
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,6 +1,6 @@
 class Collection < ActiveRecord::Base
   
-  has_many :monuments, :class_name => "Monument", :foreign_key => "colleciton_id"
+  has_many :monuments, :class_name => "Monument", :foreign_key => "collection_id"
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
   
   validates_presence_of :name, :message => "can't be blank"
