2179d076167132330535c3c2411e9536b9468e81
Luis Prill Sempere <luniprise@gmail.com>
2015-02-05 13:05:19 +0100

    added pictures methods + edit and delete links

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,18 +1,61 @@
 class PicturesController < ApplicationController
+  before_filter :find_picture, :only => [:edit, :update, :destroy]
   
-  def create
-    @picture = Picture.new(picture_params)
-    if @picture.save
-      if params[:commit] == "save picture & save monument"  #  if no more pictures should be added TO DO: find better solution
-        @monument = Monument.find(@picture.monument_id)
-        @monument.update(step: 2)  # jump to the next step
-      end
-      redirect_to edit_monument_path(id: @picture.monument_id), notice: 'You succesfully uploaded a picture.'  
-    else # some validation is missing
-      @monument = Monument.find(@picture.monument_id)
-      @monument.step = 2 # for rendering
-      render 'monuments/edit'
+  # def create
+  #   @picture = Picture.new(picture_params)
+  #   if @picture.save
+  #     if params[:commit] == "save picture & save monument"  #  if no more pictures should be added TO DO: find better solution
+  #       @monument = Monument.find(@picture.monument_id)
+  #       @monument.update(step: 2)  # jump to the next step
+  #     end
+  #     redirect_to edit_monument_path(id: @picture.monument_id), notice: 'You succesfully uploaded a picture.'
+  #   else # some validation is missing
+  #     @monument = Monument.find(@picture.monument_id)
+  #     @monument.step = 2 # for rendering
+  #     render 'monuments/edit'
+  #   end
+  # end
+  
+  # def index
+  #   @pictures = current_user.pictures
+  # end
+  
+  # def new
+  #   @picture = Picture.new(monument_id: params[:monument_id)
+  # end
+  
+  # def create
+  #   @picture = Picture.new(picture_params)
+  #   if @picture.save
+  #     redirect_to pictures_path, notice: 'You created succesfully a picture.'
+  #   else
+  #     render action: 'new'
+  #   end
+  # end
+  
+  def edit
+  end
+
+  def update
+    if @picture.update(picture_params)
+      redirect_to monument_path(@picture.monument_id), notice: 'You updated your picture successfully.'
+    else
+      render action: 'edit'
     end
+  end
+  
+  # def show
+  # end
+  
+  def destroy
+    @picture.destroy
+    redirect_to monument_path(@picture.monument_id)
+  end
+  
+  private
+  
+  def find_picture
+    @picture = Picture.find(params[:id])
   end
   
   private
