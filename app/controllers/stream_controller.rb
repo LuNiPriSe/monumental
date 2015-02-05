90747f3eb33cbcf76a69d6ca7bad957384a18fda
Luis Prill Sempere <luniprise@gmail.com>
2015-02-05 15:17:41 +0100

    added coverflow

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -4,6 +4,10 @@ class StreamController < ApplicationController
     @monuments = Monument.where(public: true, public_aproved: true).order(created_at: :asc)
   end
   
+  def orbit
+    @monuments = Monument.where(public: true, public_aproved: true).order(created_at: :asc)
+  end
+  
   def aprove
     if current_user.admin == true
       @monuments = Monument.where(public: true).order(public_aproved: :asc, created_at: :asc)
