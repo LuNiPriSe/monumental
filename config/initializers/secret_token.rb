453174195a6a557b458ccbdc6d03b619c68aaa15
Luis Prill Sempere <luniprise@gmail.com>
2015-02-05 16:00:33 +0100

    secret_token for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monumental::Application.config.secret_key_base = '697e60766465e85a32435ec9e476e83d8e56a37518cdb6dd9df8a89bbc96fcb5abc81f0ec2224f388320fa079fd1f897e3c1dee0f87d37e209a157f597067038'
