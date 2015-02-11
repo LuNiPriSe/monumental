2971270b516eb38e1a55fe4f72d1e901d5bd8ae0
Luis Prill Sempere <luniprise@gmail.com>
2015-02-11 15:01:45 +0100

    basic collections testing

--- spec/support/controller_helper_spec.rb
+++ spec/support/controller_helper_spec.rb
@@ -1,7 +1,8 @@
 # for simple authetication tests
 
 module ControllerHelpers
-   def sign_in(user = double('user'))
+   def sign_in(user = double('user', id: 1))
+      allow(user).to receive(:collections).and_return(Collection.all)   # bad hack because no user_id checked
      if user.nil?
        allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :user})
        allow(controller).to receive(:current_user).and_return(nil)
@@ -11,6 +12,11 @@ module ControllerHelpers
      end
    end
    
+   # def sign_in_as_admin(user = double('user', admin: true))
+   #    allow(user).to receive(:collections).and_return(true)
+   #    allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+   #    allow(controller).to receive(:current_user).and_return(user)
+   #  end
  end
 
  RSpec.configure do |config|
